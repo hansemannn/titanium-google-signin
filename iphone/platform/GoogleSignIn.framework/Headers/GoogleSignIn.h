df0504651ed43f49a184dc0829fb980c01e27ebc
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-08-13 17:57:55 +0200

    Update GoogleSignIn to 4.0.2

--- iphone/platform/GoogleSignIn.framework/Headers/GoogleSignIn.h
+++ iphone/platform/GoogleSignIn.framework/Headers/GoogleSignIn.h
@@ -1,13 +1,3 @@
-//
-//  GoogleSignIn.h
-//  Google Sign-In iOS SDK
-//
-//  Copyright 2015 Google Inc.
-//
-//  Use of this SDK is subject to the Google APIs Terms of Service:
-//  https://developers.google.com/terms/
-//
-
 #import "GIDAuthentication.h"
 #import "GIDGoogleUser.h"
 #import "GIDProfileData.h"
