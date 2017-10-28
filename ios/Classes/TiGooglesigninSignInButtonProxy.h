7b504a10e5e66f774d766520ddb4fcf49ff64402
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-10-28 19:40:05 +0200

    Move to ios-directory, update dependencies

---
+++ ios/Classes/TiGooglesigninSignInButtonProxy.h
@@ -0,0 +1,13 @@
+/**
+ * ti.googlesignin
+ *
+ * Created by Hans Knöchel
+ * Copyright (c) 2017 Hans Knöchel. All rights reserved.
+ */
+
+#import "TiViewProxy.h"
+
+@interface TiGooglesigninSignInButtonProxy : TiViewProxy {
+}
+
+@end
---
+++ ios/Classes/TiGooglesigninSignInButtonProxy.m
@@ -0,0 +1,13 @@
+/**
+ * ti.googlesignin
+ *
+ * Created by Hans Knöchel
+ * Copyright (c) 2017 Hans Knöchel. All rights reserved.
+ */
+
+#import "TiGooglesigninSignInButtonProxy.h"
+#import "TiUtils.h"
+
+@implementation TiGooglesigninSignInButtonProxy
+
+@end
